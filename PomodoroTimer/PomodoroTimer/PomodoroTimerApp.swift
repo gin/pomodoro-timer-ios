71071c8b2fa8d59c6c07f2899fce6f2b9be1176b
Luigi <luigi@umbrellavalidator.com>
2025-12-28 13:32:28 -0800

    Init iOS project with SwiftUI, Observation, XCTest

---
+++ PomodoroTimer/PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by luigi on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
